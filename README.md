3eaca1d6e26932d13cc4eaf5756636ac413dd389
Lucas Barque <lucasbarquedasilva@hotmail.com>
2022-03-17 20:50:12 -0400

    Subindo projeto

---
+++ README.md
@@ -0,0 +1,37 @@
+<h1 align="center"> Perform React </h1>
+
+<!-- Sobre o Projeto -->
+
+## 🧐 Sobre o Projeto
+
+Esse projeto é composto de vários exemplos de aplicação de estratégias de performance em react.
+
+São utilizadas as seguintes funções:
+
+- memo();
+- useMemo();
+- useCallback();
+- dynamic (do NextJS);
+
+## 💻 Rodando o Projeto
+
+```bash
+# Realize o Clone deste repositório
+$ https://github.com/lucasbarque/PerformReact.git
+# ou use a opção de download.
+# Acesse a pasta
+$ cd PerformReact
+
+# Rode o servidor
+$ yarn server
+
+# Inicie a aplicação
+$ yarn dev
+
+```
+
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Stat
---
+++ components/SearchResults.tsx
@@ -0,0 +1,27 @@
+import { useMemo } from "react";
+import { ProductItem } from "./ProductItem";
+
+
+interface SearchResultsProps {
+  totalPrice: number;
+  results: Array<{
+    id: number;
+    price: number;
+    priceFormatted: string;
+    title: string;
+  }>
+  onAddToWishList: (id: number) => void;
+}
+
+export function SearchResults({ results, onAddToWishList, totalPrice }: SearchResultsProps) {
+  return (
+    <div>
+      <h2>{totalPrice}</h2>
+      {results.map(product => {
+        return (
+          <ProductItem key={product.id} product={product} onAddToWishList={onAddToWishList} />
+        );
+      })}
+    </div>
+  )
+}
